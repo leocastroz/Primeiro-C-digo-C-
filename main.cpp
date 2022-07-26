5960d6d8ff6cd25a761dedbefc33e9212c875497
Leonardo Silva Castro <72839343+leocastroz@users.noreply.github.com>
2022-07-26 02:29:39 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> //necessário para usar setlocale
+#include <conio.h> // cor do caractere na tela
+using namespace std;
+int main() {
+	cout << "Hello World!" << endl << endl;
+	cout << "Estou programando em C++" <<endl << endl;
+	return 0;
+}
